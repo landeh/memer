077a2b0d2f1c442497b8fc222d2adfc7b8529c6f
Hershey Lande <hersheylande@gmail.com>
2019-07-24 22:16:46 -0700

    adding secure links

--- src/App.js
+++ src/App.js
@@ -14,24 +14,16 @@ class App extends React.Component {
   }
 
   getMemes = async (e) => {
-    // this line prevents the page from reloading (which is the default for <form> elements)
     e.preventDefault()
-    // set "loading" to true in the state so we can show a spinner
     this.setState({loading: true, memes:[]})
-    // here is our giphy api key
     var key = 'jhQazp87aPuMIRIZoFu2kaI2Uk5GjZRJ'
-    // this line make a URL string, I got this from their documentation
-    var url = `http://api.giphy.com/v1/gifs/search?q=${this.state.text}&api_key=${key}`
-    // "fetch" calls the giphy API!
+    var url = `https://api.giphy.com/v1/gifs/search?q=${this.state.text}&api_key=${key}`
     var r = await fetch(url)
-    // this lines extracts JSON (javascript object notation)
     var json = await r.json()
-    // set the memes in state, and loading to false, and the text to blank again
     this.setState({memes: json.data, loading:false, text:''})
   }
 
   render() {
-    // extract the pieces of state so we can use them easily in our HTML code
     var {memes, loading, text} = this.state
     return (
       <div className="App">
@@ -55,7 +47,6 @@ class App extends React.Component {
         {loading && <LinearProgress />}
         <main>
           {memes.map(meme=>{
-            // "map" loops over each meme and returns a <Meme> component
             return <Meme key={meme.id} meme={meme} />
           })}
         </main>
@@ -64,9 +55,6 @@ class App extends React.Component {
   }
 }
 
-// the Meme component is very simple
-// it just returns a <div> with an <img> inside it
-// the "src" of the <img> is the URL that is given to us by the giphy API
 function Meme(props){
   const {meme} = props
   const url = meme.images.fixed_height.url
